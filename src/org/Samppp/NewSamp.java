363f2bb7c3337cddc8787b5d5cb48f8a5a5ff577
unknown <dinesh062095@gmail.com>
2021-01-20 19:35:03 +0530

    SDS

--- src/org/Samppp/NewSamp.java
+++ src/org/Samppp/NewSamp.java
@@ -5,6 +5,19 @@ public class NewSamp {
 	public static void main(String[] args) {
 
 		System.out.println("Dinesh");
+		
+		String s1 = "Iphone";
+		System.out.println(s1);
+		
+		System.out.println("Gggggggggggg");
+		
+		
+		String s3 =  "Salman";
+		System.out.println(s3);
+		
+		
+		
+		
 
 	}
 
